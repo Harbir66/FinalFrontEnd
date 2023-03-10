676fc40553f0d644e89f7ebdc859a730f9a315d2
Harbir Singh <harbir066@gmail.com>
2023-03-10 17:45:09 +0530

    chore : add font and update eslintrc

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import {
+  HOME_ROUTE,
+  ERROR_ROUTE,
+  REGISTER_ROUTE,
+  LOGIN_ROUTE,
+  CONTENT_TYPES,
+} from './constants/routes';
+import {
+  Home,
+  Error,
+  PageNotFound,
+  Register,
+  Login,
+  ContentTypes,
+} from './pages';
+import ProtectedRoutes from './utils/ProtectedRoute';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path={REGISTER_ROUTE} element={<Register />} />
+          <Route path={LOGIN_ROUTE} element={<Login />} />
+          <Route path={`${ERROR_ROUTE}/:errorCode?`} element={<Error />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route
+            path={HOME_ROUTE}
+            element={
+              <ProtectedRoutes>
+                <Home />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path={`${CONTENT_TYPES}/*`}
+            element={
+              <ProtectedRoutes>
+                <ContentTypes />
+              </ProtectedRoutes>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
