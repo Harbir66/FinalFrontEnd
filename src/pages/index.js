23073d94bf713b43bbd64ed2fdc4d764370abe5f
Harbir Singh <harbir066@gmail.com>
2023-03-10 17:47:58 +0530

    chore : add update pages index file

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Error } from './Error';
+export { default as PageNotFound } from './NotFound';
+export { default as Register } from './Register';
+export { default as Login } from './Login';
+export { default as ContentTypes } from './ContentTypes';
