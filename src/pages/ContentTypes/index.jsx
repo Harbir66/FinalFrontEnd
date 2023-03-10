3310239aa7ae15ea1f217dc7dcf9253fc27c593b
Harbir Singh <harbir066@gmail.com>
2023-03-10 17:48:32 +0530

    feat : add content type component

---
+++ src/pages/ContentTypes/index.jsx
@@ -0,0 +1,299 @@
+import React from 'react';
+import './ContentType.css';
+import { useNavigate } from 'react-router-dom';
+import {
+  Sidebar,
+  Header,
+  EntriesCount,
+  DottedButton,
+  DisplayButton,
+  FieldEntry,
+  EntriesHeader,
+  ContentEntries,
+  ModalForm,
+} from '../../components';
+import makeRequest from '../../utils/makeRequest';
+import {
+  CREATE_NEW_COLLECTION,
+  CREATE_NEW_FIELD,
+  DELETE_FIELD,
+  GET_ALL_DATA,
+  GET_ALL_ENTRIES,
+  UPDATE_FIELD,
+} from '../../constants/apiEndPoints';
+import { getCollectionNamesFromData } from '../../utils/common';
+import editIcon from '../../assets/user-pencil-write-ui-education@3x.png';
+
+function ContentTypes() {
+  const [allData, setAllData] = React.useState([]);
+  const [view, setView] = React.useState('main');
+  const [selectedContentId, setSelectedContentId] = React.useState(null);
+  const [selectedCollectionId, setSelectedCollectionId] = React.useState(null);
+  const [entries, setEntries] = React.useState([]);
+  const [isOpen, setIsOpen] = React.useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+  const [formInput, setFormInput] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormInput('');
+    closeModal();
+  };
+
+  //   const [error, setError] = React.useState(null);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    makeRequest(GET_ALL_DATA, {}, navigate).then((data) => {
+      setAllData(data);
+      setSelectedContentId(data[0].id);
+      setSelectedCollectionId(data[0].id);
+    });
+    // .catch((err) => {
+    //   console.log(err);
+    // });
+  }, []);
+  // console.log(allData);
+
+  const handleViewChange = (newView) => {
+    setView(newView);
+  };
+  const handleContentTypeChange = (newContentId) => {
+    setSelectedContentId(newContentId);
+  };
+
+  const handleFieldDelete = async (field) => {
+    try {
+      const response = await makeRequest(
+        DELETE_FIELD(selectedContentId),
+        { data: { field } },
+        navigate
+      );
+      if (response) {
+        const newAllData = allData.map((collection) => {
+          if (collection.id === selectedContentId) {
+            const newFields = collection.fields.filter((f) => f !== field);
+            return { ...collection, fields: newFields };
+          }
+          return collection;
+        });
+        setAllData(newAllData);
+      }
+    } catch (err) {
+      //
+    }
+  };
+
+  const handleNewType = async (input) => {
+    try {
+      const response = await makeRequest(
+        CREATE_NEW_COLLECTION,
+        { data: { contentType: input } },
+        navigate
+      );
+      if (response) {
+        const newAllData = [...allData, response.data];
+        setAllData(newAllData);
+        setSelectedContentId(response.data.id);
+      }
+    } catch (err) {
+      //
+    }
+  };
+
+  const handleFieldAdd = async (field) => {
+    try {
+      const response = await makeRequest(
+        CREATE_NEW_FIELD(selectedContentId),
+        { data: { field } },
+        navigate
+      );
+      if (response) {
+        const newAllData = allData.map((collection) => {
+          if (collection.id === selectedContentId) {
+            const newFields = [...collection.fields, field];
+            return { ...collection, fields: newFields };
+          }
+          return collection;
+        });
+        setAllData(newAllData);
+      }
+    } catch (err) {
+      //
+    }
+  };
+
+  const handleFieldRename = async (field, newField) => {
+    try {
+      const response = await makeRequest(
+        UPDATE_FIELD(selectedContentId),
+        { data: { field, newField } },
+        navigate
+      );
+      if (response) {
+        const newAllData = allData.map((collection) => {
+          if (collection.id === selectedContentId) {
+            const newFields = collection.fields.map((currentField) =>
+              currentField === field ? newField : currentField
+            );
+            return { ...collection, fields: newFields };
+          }
+          return collection;
+        });
+        setAllData(newAllData);
+      }
+    } catch (err) {
+      //
+    }
+  };
+
+  const handleCollectionChange = async (newCollectionId) => {
+    setSelectedCollectionId(newCollectionId);
+    const response = await makeRequest(
+      GET_ALL_ENTRIES(newCollectionId),
+      {},
+      navigate
+    );
+    setEntries(response.data.values);
+  };
+
+  if (allData.length === 0) return <div>Loading...</div>;
+
+  const collections = getCollectionNamesFromData(allData);
+
+  const selectedContentName = collections.find(
+    (collection) => collection.id === selectedContentId
+  ).name;
+  const selectedContentFields = collections.find(
+    (collection) => collection.id === selectedContentId
+  ).fields;
+  const selectedContentAllFields = collections.find(
+    (collection) => collection.id === selectedContentId
+  ).allFields;
+
+  const selectedCollectionName = collections.find(
+    (collection) => collection.id === selectedCollectionId
+  ).name;
+  const selectedCollectionValues = collections.find(
+    (collection) => collection.id === selectedCollectionId
+  ).values;
+  const selectedCollectionAllFields = collections.find(
+    (collection) => collection.id === selectedCollectionId
+  ).allFields;
+  const heading = view === 'main' ? 'Content Types' : selectedCollectionName;
+
+  const modalForm = (
+    <ModalForm isOpen={isOpen} onClose={closeModal}>
+      <h1>Rename the Field</h1>
+      <form>
+        <label htmlFor="name">
+          New name of the Field
+          <input
+            type="text"
+            id="name"
+            value={formInput}
+            onChange={(e) => setFormInput(e.target.value)}
+          />
+        </label>
+        <button type="button" onClick={closeModal}>
+          Cancel
+        </button>
+        <button onClick={handleSubmit} type="submit">
+          Create
+        </button>
+      </form>
+    </ModalForm>
+  );
+  return (
+    <div className="content-type">
+      <Sidebar
+        collections={collections || []}
+        handleViewChange={handleViewChange}
+        handleCollectionChange={handleCollectionChange}
+      />
+      <div className="main-body">
+        <Header heading={heading} />
+        {view === 'main' ? (
+          <div className="content-type-builder">
+            <div className="content-types">
+              <EntriesCount
+                count={collections.length}
+                type="Types"
+                showSearch
+              />
+              <DottedButton
+                handleNew={handleNewType}
+                text="+ New Type"
+                large={false}
+              />
+              <div className="button-container">
+                {collections.map((collection) => (
+                  <DisplayButton
+                    key={collection.id}
+                    text={collection.name}
+                    count={collection.fields}
+                    selected={selectedContentId === collection.id}
+                    onClick={() => handleContentTypeChange(collection.id)}
+                  />
+                ))}
+              </div>
+            </div>
+            <div className="content-type-fields">
+              <span className="content-type-header">
+                <h1>{selectedContentName}</h1>
+                <button type="button">
+                  <img src={editIcon} alt="edit-button" />
+                </button>
+              </span>
+              <EntriesCount
+                count={selectedContentFields}
+                type="Fields"
+                showSearch={false}
+              />
+              <DottedButton
+                handleNew={handleFieldAdd}
+                text="Add another field"
+                large
+              />
+              <div className="fields-container">
+                {selectedContentAllFields.map((field) => (
+                  <FieldEntry
+                    key={field}
+                    fieldName={field}
+                    handleFieldDelete={handleFieldDelete}
+                    handleFieldRename={handleFieldRename}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="entries">
+            <div>
+              <span>{selectedCollectionValues} Entries Found</span>
+              <button onClick={openModal} type="button">
+                Add new
+              </button>
+            </div>
+            <EntriesHeader fields={selectedCollectionAllFields} />
+            <div className="entries-container">
+              {entries.map((entry, index) => (
+                <ContentEntries
+                  key={Math.random()}
+                  index={index}
+                  entry={entry}
+                  fields={selectedCollectionAllFields}
+                />
+              ))}
+            </div>
+            <div />
+          </div>
+        )}
+      </div>
+      {modalForm}
+    </div>
+  );
+}
+
+export default ContentTypes;
