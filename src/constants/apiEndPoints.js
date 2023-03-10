68e830f195ff7203962236d75f0a95571a0a43d7
Harbir Singh <harbir066@gmail.com>
2023-03-10 17:49:32 +0530

    chore : update api-endpoints

---
+++ src/constants/apiEndPoints.js
@@ -0,0 +1,31 @@
+export const BACKEND_URL = 'http://localhost:3020/api';
+
+export const GET_ALL_DATA = {
+  url: 'collections',
+  method: 'GET',
+};
+
+export const DELETE_FIELD = (collectionId) => ({
+  url: `fields/${collectionId}`,
+  method: 'DELETE',
+});
+
+export const CREATE_NEW_COLLECTION = {
+  url: 'collections',
+  method: 'POST',
+};
+
+export const CREATE_NEW_FIELD = (collectionId) => ({
+  url: `fields/${collectionId}`,
+  method: 'POST',
+});
+
+export const UPDATE_FIELD = (fieldId) => ({
+  url: `fields/${fieldId}`,
+  method: 'PATCH',
+});
+
+export const GET_ALL_ENTRIES = (collectionId) => ({
+  url: `entries/${collectionId}`,
+  method: 'GET',
+});
